869f0c3ff395f461f598950ab70e0fbeed93b75e
Pedro Almeida <pedroalmeida199620@gmail.com>
2021-11-24 22:59:51 +0100

    initial routing and view for tweet

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -1,21 +1,11 @@
-"""tweetme2 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""tweetme2 URL Configuration"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path  # url()
+from tweets.views import home_view
+from tweets.views import tweet_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+    path('tweets/<int:tweet_id>', tweet_detail_view)
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Tweet
 
-# Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello world</h1>")
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
+
+    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
