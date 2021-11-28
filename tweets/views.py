bdfb09e1608b85618cf0904609302e9444dc9beb
Pedro Almeida <pedroalmeida199620@gmail.com>
2021-11-28 21:29:37 +0100

    created REST endpoints

--- tweets/views.py
+++ tweets/views.py
@@ -1,16 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello world</h1>")
+    # return HttpResponse("<h1>Hello world</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+        REST API VIEW
+        Consumed by JS or Swift or Java or iOS/Android, etc.
+        return JSON data
+    """
+
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+
+    return JsonResponse(data)
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+        REST API VIEW
+        Consumed by JS or Swift or Java or iOS/Android, etc.
+        return JSON data
+    """
+
+    data = {
+        "id": tweet_id,
+        # "image_path": obj.image.url
+    }
+
+    status = 200
+
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
     except:
-        raise Http404
+        data['message'] = "Not found"
+        status = 404
 
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+    # json.dumps content_type)='application/json'
+    return JsonResponse(data, status=status)
