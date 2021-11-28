97b4f533eab0a9db3b50515b5463168a874760a9
Pedro Almeida <pedroalmeida199620@gmail.com>
2021-11-28 21:30:10 +0100

    created new views

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -14,6 +14,7 @@ import os
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# remember on same level as manage.py
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'tweetme2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -1,11 +1,11 @@
 """tweetme2 URL Configuration"""
 from django.contrib import admin
 from django.urls import path, re_path  # url()
-from tweets.views import home_view
-from tweets.views import tweet_detail_view
+from tweets.views import home_view, tweet_list_view, tweet_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view)
 ]
